bdcb7b687e28b1ebdaab601ade6cc9fe4e9d0f26
douglas86 <douglasmaxton@gmail.com>
2021-01-13 23:38:21 +0200

    Gotten the creator to store to db

--- server/models/postMessage.js
+++ server/models/postMessage.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const postSchema = mongoose.Schema({
     title: String,
     message: String,
-    creater: String,
+    creator: String,
     tags: [String],
     selectedFile: String,
     likeCount: {
