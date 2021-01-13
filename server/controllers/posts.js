ffc86a1039a3f27adefc4eb7198add398a91b866
douglas86 <douglasmaxton@gmail.com>
2021-01-13 18:07:53 +0200

    created some logic for controllers in posts.js

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,17 @@
-export const getPosts = (req, res) => {
-    res.send('This works!');
+import PostMessage from '../models/postMessage.js';
+
+export const getPosts = async (req, res) => {
+    try {
+        const postMessages = await PostMessage.find();
+
+        console.log(postMessages);
+
+        res.status(200).json(postMessages);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+};
+
+export const createPost = (req, res) => {
+    res.send('Post Creation');
 };
