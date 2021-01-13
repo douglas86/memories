74d296641eca167b82940234f214a1665c893136
douglas86 <douglasmaxton@gmail.com>
2021-01-13 20:15:40 +0200

    Added some styling

--- client/src/App.js
+++ client/src/App.js
@@ -5,14 +5,31 @@ import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
 import memories from './images/memories.jpg';
 
+import useStyles from './styles';
+
 const App = () => {
+    const classes = useStyles();
+
     return (
         <Container maxWidth="lg">
-            <AppBar position="static" color="inherit">
-                <Typography variant="h2" align="center">
+            <AppBar
+                className={classes.appBar}
+                position="static"
+                color="inherit"
+            >
+                <Typography
+                    className={classes.heading}
+                    variant="h2"
+                    align="center"
+                >
                     Memories
                 </Typography>
-                <img src={memories} alt="memories" height="60" />
+                <img
+                    className={classes.image}
+                    src={memories}
+                    alt="memories"
+                    height="60"
+                />
             </AppBar>
             <Grow in>
                 <Container>
