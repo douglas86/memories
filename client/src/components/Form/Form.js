1e145cc149cf0aa92336c0ff17b25ade8b1ffb95
douglas86 <douglasmaxton@gmail.com>
2021-01-14 15:37:00 +0200

    Handling clear on form submit

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -34,8 +34,18 @@ const Form = ({ currentId, setCurrentId }) => {
         } else {
             dispatch(createPost(postData));
         }
+        clear();
     };
-    const clear = () => {};
+    const clear = () => {
+        setCurrentId(null);
+        setPostData({
+            creator: '',
+            title: '',
+            message: '',
+            tags: '',
+            selectedFile: '',
+        });
+    };
 
     return (
         <Paper className={classes.paper}>
