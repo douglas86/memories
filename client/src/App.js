c607e5e2844e180ecd610f901723b886acc89f3b
douglas86 <douglasmaxton@gmail.com>
2021-01-14 18:57:01 +0200

    Create a constants componet for strings

--- client/src/App.js
+++ client/src/App.js
@@ -44,6 +44,7 @@ const App = () => {
                 <Container>
                     <Grid
                         container
+                        className={classes.mainContainer}
                         justify="space-between"
                         alignItems="stretch"
                         spacing={3}
